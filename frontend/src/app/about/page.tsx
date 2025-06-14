751692722d4e19a4fed71ea19e9fc23dccbf0359
Blaize Lahman <blaizelahman@gmail.com>
2025-06-14 00:42:38 -0400

    added stripe TODO

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -9,12 +9,16 @@ export default function AboutPage() {
     <div className="pt-24 px-6 space-y-12 max-w-3xl mx-auto">
       <section className="flex flex-col items-center space-y-4">
         <h1 className="text-4xl font-bold">About</h1>
+        <h1 className="text-l font-semibold">
+          No more constantly checking Search & Enroll!
+        </h1>
         <p>
-          EnrollAlert helps UW-Madison students track real-time enrollment
-          status for their courses and get notified when seats open up. All it
-          takes is an email and a click! Course section information is updated
-          when UW Course Search & Enroll is, so you will receive notifications
-          as soon as seats open up.
+          EnrollAlert gives UW-Madison students peace of mind by allowing them to 
+          track real-time enrollment status for their courses and get notified when
+          seats become available. 
+          <strong> All it takes is an email and a click! </strong>
+          Course section information is updated along with UW Course Search & Enroll,
+          so you will receive notifications as soon as seats open up. 
         </p>
 
         <hr className="border-t border-gray-200 my-12 w-full" />
@@ -23,8 +27,8 @@ export default function AboutPage() {
         <p>
           Feedback is welcome! This website is a work in progress and is built by one
           CS student, so it is by no means perfect and improvements can always be made.
-          If you have any feedback or notes, from anything to new features or how the site
-          looks, please feel free to fill out a feedback form.
+          If you have any feedback, bug fixes, feature suggestions, or anything else,
+          please feel free to fill out a feedback form.
         </p>
         <Button asChild>
           <Link 
@@ -38,6 +42,7 @@ export default function AboutPage() {
 
         <hr className="border-t border-gray-200 my-12 w-full" />
 
+        {// TODO: add stripe functionality once site is live}
         <h1 className="text-3xl font-bold">Support</h1>
         <p>
           If you feel like this site has helped you and you would like to contribute to

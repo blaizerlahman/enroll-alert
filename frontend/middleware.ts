d20162e2b987bb294ba56005147e0196ee2ed7ee
Blaize Lahman <blaizelahman@gmail.com>
2025-06-17 13:52:51 -0500

    implemented rate limiting for notifications/sections/discussions APIs

---
+++ frontend/middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { RateLimiterMemory } from 'rate-limiter-flexible'
+
+const limiter = new RateLimiterMemory({ points: 10, duration: 60 })
+
+// prevent any API call spam with more than 20 calls within a minute
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl
+
+  // keep track of any calls to notifications DELETE endpoint, or sections/discussions APIs
+  if (
+    (pathname === '/api/notifications' && req.method === 'DELETE') ||
+    pathname.startsWith('/api/sections') ||
+    pathname.startsWith('/api/discussions')
+  ) {
+    const ip = req.ip ?? req.headers.get('x-forwarded-for') ?? 'unknown'
+    try {
+      await limiter.consume(ip)
+    } catch {
+      return new NextResponse('Too Many Requests', { status: 429 })
+    }
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    '/api/sections/:path*',
+    '/api/discussions/:path*',
+    '/api/notifications'   
+  ],
+}
+

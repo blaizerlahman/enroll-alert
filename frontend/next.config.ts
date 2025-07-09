ac9e160874f5fc3ca95b3e4e52b6d8db41bda861
Blaize Lahman <87739992+blaizerlahman@users.noreply.github.com>
2025-07-09 13:40:12 -0500

    fix courses redirect

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',          
+        destination: '/courses',
+        permanent: true,      
+      },
+    ];
+  },
 };
 
 export default nextConfig;

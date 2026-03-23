5768bb26949aef54d0c94a59f6dd9da797176691
Blaize Lahman <87739992+blaizerlahman@users.noreply.github.com>
2026-03-23 13:14:57 -0500

    refactored config to allow for multiple terms to be input as comma-separated string of ints

--- backend/cmd/lambda/main.go
+++ backend/cmd/lambda/main.go
@@ -10,32 +10,42 @@ import (
 	"os"
 	"strconv"
 	"sync"
+	"strings"
 	"time"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+const (
+	defaultInit   	 = false
+	defaultCount  	 = 5666
+	defaultTerms  	 = "1266,1272"
+	defaultBatchSize = 50
+	defaultDelayTime = 10
+)
+
 var (
-	initFlag      = flag.Bool("init", false, "")
-	countFlag     = flag.Int("count", 5666, "")
-	termFlag      = flag.Int("term", 1264, "")
-	batchSizeFlag = flag.Int("batchsize", 50, "")
-	delayTimeFlag = flag.Int("delay", 10, "")
+	initFlag      = flag.Bool("init", defaultInit, "")
+	countFlag     = flag.Int("count", defaultCount, "")
+	termsFlag     = flag.String("terms", defaultTerms, "")
+	batchSizeFlag = flag.Int("batchsize", defaultBatchSize, "")
+	delayTimeFlag = flag.Int("delay", defaultDelayTime, "")
 	parseOnce     sync.Once
 )
 
 type Config struct {
 	init        bool
 	count       int
-	term        int
+	terms       []int
 	batchSize   int
 	delayTime   int
 	postgresURL string
 }
 
-// envBool Parse boolean flag for given input.
+// envBool Parse boolean env variable for given input.
 // Return input boolean or default if no given input.
 func envBool(search string, defaultFlag bool) bool {
+
 	if flag, ok := os.LookupEnv(search); ok {
 		boolFlag, _ := strconv.ParseBool(flag)
 		return boolFlag
@@ -43,9 +53,10 @@ func envBool(search string, defaultFlag bool) bool {
 	return defaultFlag
 }
 
-// envInt Parse integer flag for given input.
+// envInt Parse integer env variable for given input.
 // Return input integer or default if no given input
 func envInt(search string, defaultFlag int) int {
+
 	if flag, ok := os.LookupEnv(search); ok {
 		intFlag, _ := strconv.Atoi(flag)
 		return intFlag
@@ -53,14 +64,50 @@ func envInt(search string, defaultFlag int) int {
 	return defaultFlag
 }
 
+// envString Parse string env variable for given input.
+// Return input string or default if no given input
+func envString(search string, defaultFlag string) string {
+	
+	if flag, ok := os.LookupEnv(search); ok && (flag != "") {
+		return flag
+	}
+	return defaultFlag
+}
+
+// parseTerms Parses a comma-separated string of term numbers into a slice of ints.
+// Return A slice of input term ints or empty list if parse fails
+func parseTerms(terms string) []int {
+
+	var termInts []int
+	for _, part := range strings.Split(terms, ",") {
+		term, err := strconv.Atoi(strings.TrimSpace(part))
+		if err != nil {
+			continue
+		}
+		termInts = append(termInts, term)
+	}
+
+	return termInts
+}
+
 // loadConfig Parse input flags and load DB URL.
 // Return config object containing flags and DB URL.
 func loadConfig() Config {
+
 	parseOnce.Do(flag.Parse)
+
+	var parsedTermsFlag []int
+
+	// parse terms, resort to default if parsing fails
+	parsedTermsFlag = parseTerms(envString("TERMS", *termsFlag))
+	if len(parsedTermsFlag) == 0 {
+		parsedTermsFlag = parseTerms(defaultTerms)
+	}
+
 	return Config{
 		init:        envBool("INIT", *initFlag),
 		count:       envInt("COUNT", *countFlag),
-		term:        envInt("TERM", *termFlag),
+		terms:       parsedTermsFlag,
 		batchSize:   envInt("BATCHSIZE", *batchSizeFlag),
 		delayTime:   envInt("DELAYTIME", *delayTimeFlag),
 		postgresURL: os.Getenv("POSTGRES_URL"),
@@ -72,8 +119,6 @@ func loadConfig() Config {
 // if new course info satisfies their conditions for an alert.
 // Return error if error encountered during scraping
 func run(ctx context.Context, config Config) error {
-	enrollalert.TermNum = config.term
-	enrollalert.Term = strconv.Itoa(config.term)
 
 	// run initial DB loading if specified
 	if config.init {
@@ -119,6 +164,7 @@ func run(ctx context.Context, config Config) error {
 }
 
 func getOutboundIP(ctx context.Context) (string, error) {
+
 	req, err := http.NewRequestWithContext(ctx, "GET", "https://api.ipify.org", nil)
 	if err != nil {
 		return "", err
